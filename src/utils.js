4f02b9ee21cdb2c95baf40be238b157293f96ab8
Илья <70086932+Gorvarydar@users.noreply.github.com>
2023-05-09 13:07:18 +0300

    init commit

---
+++ src/utils.js
@@ -0,0 +1,10 @@
+import {unlink} from 'fs/promises'
+
+export async function removeFile (path) {
+    try{
+        await unlink(path)
+
+    } catch(e) {
+        console.log('error while file is removing', e.message)
+    }
+}
